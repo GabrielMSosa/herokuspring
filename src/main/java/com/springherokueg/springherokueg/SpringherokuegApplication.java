9bd12edf37dcabdd6e95fecdf3ec677b9aa7959b
GabrielMSosa <sosagabriel79@gmail.com>
2022-04-22 16:02:53 -0300

    1ercommit

---
+++ src/main/java/com/springherokueg/springherokueg/SpringherokuegApplication.java
@@ -0,0 +1,13 @@
+package com.springherokueg.springherokueg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringherokuegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringherokuegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springherokueg/springherokueg/SpringherokuegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springherokueg.springherokueg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringherokuegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
